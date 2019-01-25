fb6c3f07c8e35cb5ebcae507810ea2d570ebd0bb
Mostafa Salama <mostafalearning2016@gmail.com>
2019-01-25 15:24:40 +0200

    adding example for block scope in JS with const and let

--- scope/index.js
+++ scope/index.js
@@ -15,3 +15,19 @@ function local() {
     console.log(localVariable) ;
 }
 console.log(localVariable) // ReferenceError: localVariable is not defined
+
+// its better to use ""let"" for blocking scope
+function f() {
+    for(let i = 0 ; i<10;i++){
+        let protocol = 'http' ; // this is block scope
+    }
+}
+/*
+   using const
+   now you can't assign values to that variable
+ */
+
+const BASE_URL = 'https://www.google.com';
+
+BASE_URL = 'http://www.github.com'; // TypeError: Assignment to constant variable
+
