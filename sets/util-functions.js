21b1cecc6325146d0ce05bd779f9be594f340a15
Mostafa Salama <mostafalearning2016@gmail.com>
2019-01-26 20:36:17 +0200

    adding isSuperSet function

--- sets/util-functions.js
+++ sets/util-functions.js
@@ -5,15 +5,30 @@
  * @return {Set} a set with the intersected elements in the two sets
  */
 function intersectSets(setA, setB) {
-    const intersection = new Set();
-    for (let element of setB) {
-        if (setA.has(element)) {
-            intersection.add(element);
-        }
-    }
-    return intersection;
+	const intersection = new Set();
+	for (let element of setB) {
+		if (setA.has(element)) {
+			intersection.add(element);
+		}
+	}
+	return intersection;
 }
 
+/**
+ *  function takes two sets and return true
+ *  if the second is a subset of the first
+ *  else returns false
+ * @param setA {Set}
+ * @param subset {Set}
+ * @return {Boolean}
+ */
+function isSuperSet(setA, subset) {
+	for (let element of subset) {
+		if (!setA.has(element)) return false;
+	}
+	return true;
+}
 module.exports = {
-    intersectSets,
+	intersectSets,
+	isSuperSet,
 };
