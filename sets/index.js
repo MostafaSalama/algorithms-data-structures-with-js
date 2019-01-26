93cb85303f7a9b95841a52ded1f2e39e9a250d2d
Mostafa Salama <mostafalearning2016@gmail.com>
2019-01-26 20:24:18 +0200

    operations in sets example

--- sets/index.js
+++ sets/index.js
@@ -12,3 +12,17 @@ set.add(5); // this will not be added
 set.forEach(value => {
 	console.log(value);
 });
+
+// operations in sets
+
+// adding values
+
+set.add('this is a new value') ;
+
+// to remove
+
+set.delete(3) ;
+
+// to contain specific value
+
+set.has(5) ; // O(1)
