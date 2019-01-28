31b9cced9336056e86c22d4f6dd6ab1f2f07cfc3
Mostafa Salama <mostafalearning2016@gmail.com>
2019-01-28 14:51:04 +0200

    adding binary search algorithm function

--- searching_sorting/search.js
+++ searching_sorting/search.js
@@ -20,3 +20,29 @@ function linearSearch(array, n) {
 
 console.log(linearSearch([4, 5, 65, 6, 7, 12], 7)); // true
 console.log(linearSearch([4, 5, 65, 6, 7, 12], 9)); // false
+
+// a better search with binary search O(log(n))
+/**
+ *  a simple binary search function
+ *  returns the index of the element if it exists in the array
+ *  otherwise returns -1
+ * @param array {Array}
+ * @param n
+ * @returns {number}
+ */
+function binarySearch(array, n) {
+	let lowIndex = 0,
+		highIndex = array.length - 1;
+	while (lowIndex <= highIndex) {
+		let midIndex = Math.floor((highIndex + lowIndex) / 2);
+		if (array[midIndex] === n) {
+			return midIndex;
+		} else if (n > array[midIndex]) {
+			lowIndex = midIndex;
+		} else {
+			highIndex = midIndex;
+		}
+	}
+	return -1;
+}
+console.log(binarySearch([2,5,6,7,8,9,13],6)) ;
