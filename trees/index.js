b7d3f55ebf8656582347ae27d42ab492ce88d13e
Mostafa Salama <mostafalearning2016@gmail.com>
2019-02-03 04:29:05 +0200

    adding some terms for tree DS

---
+++ trees/index.js
@@ -0,0 +1,18 @@
+/*
+    what is tree
+    hierarchical data structures
+    nodes
+    top node => root
+    parent adn children
+ */
+
+/*
+    Binary Tree
+    2 children at most
+ */
+
+function TreeNode(value) {
+    this.left = null ;
+    this.right = null ;
+    this.value = value ; 
+}
