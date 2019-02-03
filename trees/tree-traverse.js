2749c4cccb9a7f39bd44bd155345f41ed5fb7c28
Mostafa Salama <mostafalearning2016@gmail.com>
2019-02-03 04:49:20 +0200

    explain a terms for traverse a tree DS

---
+++ trees/tree-traverse.js
@@ -0,0 +1,11 @@
+/*
+
+	different way to traverse a tree
+
+	InOrder Left => Root => Right
+	PreOrder Root => Left => Right
+	PostOrder Left => Right => Root
+
+	BF Breadth First or Level Order
+ */
+
