5c00d0d757e3e9e85b79bccc74951269c022b5e3
Mostafa Salama <mostafalearning2016@gmail.com>
2019-02-03 04:33:00 +0200

    adding simple binary tree example

--- trees/index.js
+++ trees/index.js
@@ -14,5 +14,12 @@
 function TreeNode(value) {
     this.left = null ;
     this.right = null ;
-    this.value = value ; 
+    this.value = value ;
 }
+// A simple tree
+const root = new TreeNode(10) ;
+root.left = new TreeNode(2) ;
+root.right = new TreeNode(15) ;
+root.left.left = new TreeNode(11) ;
+
+console.log(root) ;
