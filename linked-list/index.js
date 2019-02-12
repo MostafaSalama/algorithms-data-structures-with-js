926d5603e4a1c11d476b38c38b8bc39d93cbb229
Mostafa Salama <mostafalearning2016@gmail.com>
2019-02-12 11:05:22 +0200

    add traverse method to the linked list with example

--- linked-list/index.js
+++ linked-list/index.js
@@ -13,6 +13,18 @@ function LinkedList() {
 
 }
 
+LinkedList.prototype.traverse = function(cb){
+    let current = this.head ;
+    while (current!== null) {
+        cb(current.value) ;
+        current = current.next ; 
+    }
+}
 let linkedList = new LinkedList() ;
 linkedList.head = new Node(2) ;
 linkedList.head.next = new Node(3) ;
+linkedList.head.next.next = new Node(3) ;
+
+linkedList.traverse((value)=>{
+    console.log(`value is ${value}`)
+})
